effb5eac211ef6f0f113940f3cae6ff24deacf3d
giulio-giunta <giuntalio@gmail.com>
2021-01-31 13:14:25 +0000

    changed instance type

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -27,7 +27,7 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t3.small"
+      instance_type                 = "t2.micro"
       additional_userdata           = "echo foo bar"
       asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
