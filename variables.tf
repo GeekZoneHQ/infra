2de88ab614a39de357cbb31e2e0bc2d7b031826b
Balamurugan Balasubramanian <kbcbala@gmail.com>
2021-07-11 09:40:45 +0100

    removed the version setting in versions.tf

--- variables.tf
+++ variables.tf
@@ -25,7 +25,8 @@ variable "map_users" {
   ]
 } 
 
+ 
 variable "db_name" {}     #Database name
 variable "db_username" {} #Database username
 variable "db_password" {} #Database password
-variable "db_port" {}     #Database port
+variable "db_port" {}     #Database port 
