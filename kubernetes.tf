d9b31d161cdd6f805eb4a5166bd2ac0235efc940
Balamurugan.Balasubramanian <kbcbala@gmail.com>
2021-11-06 21:24:12 +0000

    added the name of K8S namespace

--- kubernetes.tf
+++ kubernetes.tf
@@ -5,7 +5,7 @@
 # The Kubernetes provider is included in this file so the EKS module can complete successfully. Otherwise, it throws an error when creating `kubernetes_config_map.aws_auth`.
 # You should **not** schedule deployments and services in this workspace. This keeps workspaces modular (one for provision EKS, another for scheduling Kubernetes resources) as per best practices.
 
-provider "kubernetes" {
+/* provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   exec {
@@ -35,8 +35,9 @@ resource "kubernetes_namespace" "n" {
   metadata{    
       /* name = var.k8snamespace */
       /* default = "123456" */
-  }
+  /* }
   timeouts {
     delete = "5m"
   }
-}
+} 
+*/ 
