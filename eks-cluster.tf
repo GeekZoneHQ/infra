5a434dc252b5ae458dda229e7c0999f479ffb23e
Balamurugan.Balasubramanian <kbcbala@gmail.com>
2021-11-06 22:35:40 +0000

    added the name of K8S namespace

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -73,7 +73,7 @@ data "aws_eks_cluster_auth" "cluster" {
 
 resource "kubernetes_namespace" "example" {
   metadata {          
-      name = "${PR_NUMBER}"-"${CIRCLE_PROJECT_REPONAME}"      
+      name = ${PR_NUMBER}-${CIRCLE_PROJECT_REPONAME}
   }
   timeouts {
     delete = "5m"  
