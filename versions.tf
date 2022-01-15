8ea0309e02925caef3e65c07d404069b0227821c
Balamurugan.Balasubramanian <kbcbala@gmail.com>
2022-01-15 18:35:11 +0000

    added the upgrade for terraform

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
-    aws = {
+    /* aws = {
       source  = "hashicorp/aws"
       version = ">= 3.0.0"
-    }
+    } */
 /*
   random = {
       source  = "hashicorp/random"
