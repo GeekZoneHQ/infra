ecae396af75e3e2874b7f96edccfe120fb5465e6
giulio-giunta <56529931+giulio-giunta@users.noreply.github.com>
2022-03-23 13:42:20 +0000

    This removes a security group, removes commented lines in circleci config and adds code lint config

--- aws/terraform-aws/security-groups.tf
+++ aws/terraform-aws/security-groups.tf
@@ -45,16 +45,4 @@ resource "aws_security_group" "all_worker_mgmt" {
   }
 }
 
-resource "aws_security_group" "ssh-access" {
-  vpc_id = module.vpc.vpc_id
 
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
-  }
-}
