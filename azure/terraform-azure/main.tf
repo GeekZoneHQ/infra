e7c816ac1599b25720c08d9fc56bb9350b7c082e
giulio-giunta <56529931+giulio-giunta@users.noreply.github.com>
2023-03-19 11:50:17 +0000

    Test

--- azure/terraform-azure/main.tf
+++ azure/terraform-azure/main.tf
@@ -7,6 +7,19 @@ terraform {
       name = "dev-azure"
     }
   }
+
+  required_version = ">= 1.2"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.40, < 4.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.1"
+    }
+  }
 }
 
 provider "azurerm" {
--- azure/terraform-azure/versions.tf
+++ azure/terraform-azure/versions.tf
@@ -1,14 +1,14 @@
-terraform {
-  required_version = ">= 1.2"
+# terraform {
+#   required_version = ">= 1.2"
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.40, < 4.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 3.1"
-    }
-  }
-}
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = ">= 3.40, < 4.0"
+#     }
+#     tls = {
+#       source  = "hashicorp/tls"
+#       version = ">= 3.1"
+#     }
+#   }
+# }
